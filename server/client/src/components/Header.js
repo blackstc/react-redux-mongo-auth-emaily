198ffd0f5f397f1cff679dd7e137d29b437df862
Charlie Blackstock <charlieblackstock@gmail.com>
2017-09-04 12:38:05 -0600

    wip

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,20 +1,41 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+  renderContent() {
+    const actions = {
+      null: () => {
+        return;
+      },
+      false: () => (
+        <li>
+          <a href="auth/google">Login With Google</a>
+        </li>
+      ),
+      default: () => (
+        <li>
+          <a href="api/logout">Logout</a>
+        </li>
+      )
+    };
+
+    return (actions[this.props.auth] || actions.default)();
+  }
+
   render() {
     return (
       <nav>
         <div className="nav-wrapper">
           <a className="left brand-logo">Emaily</a>
-          <ul className="right">
-            <li>
-              <a>Login With Google</a>
-            </li>
-          </ul>
+          <ul className="right">{this.renderContent()}</ul>
         </div>
       </nav>
     );
   }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
--- server/client/src/reducers/authReducer.js
+++ server/client/src/reducers/authReducer.js
@@ -1,6 +1,9 @@
-export default function(state, action) {
+import { FETCH_USER } from '../actions/types';
+
+export default function(state = null, action) {
   const actions = {
-    default: () => []
+    [FETCH_USER]: () => action.payload || false,
+    default: () => state
   };
 
   return (actions[action.type] || actions.default)();
